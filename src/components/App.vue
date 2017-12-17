5aef0e4124b01e722375737e1f727efbcfe009f3
Kia Farhang <kfarhang0@gmail.com>
2017-12-17 13:49:41 -0600

    Add top-level App component

---
+++ src/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+      <podcast-list/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import PodcastList from "./PodcastList.vue";
+
+export default Vue.extend({
+  components: {
+    PodcastList
+  }
+});
+</script>
