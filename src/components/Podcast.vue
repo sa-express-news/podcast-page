6e8b02d1790ffc78dc692050a44baf201de77561
Kia Farhang <kfarhang0@gmail.com>
2017-12-17 13:10:08 -0600

    Add basic Podcast component

---
+++ src/components/Podcast.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="podcast">
+      <img :src="image" :alt="title"/>
+      <h3>{{ title }}</h3>
+      <p>{{ description }}</p>
+      <div class="links">
+        <p>Listen On:</p>
+        <p :v-for="link in links"><a :href="link.url">{{ link.name }}</a></p>
+      </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    image: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    links: {
+      type: Array,
+      required: true
+    }
+  }
+});
+</script>
