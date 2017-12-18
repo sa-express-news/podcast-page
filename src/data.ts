896f72da8d396247c142328044148577c2646933
Kia Farhang <kfarhang0@gmail.com>
2017-12-18 11:39:58 -0600

    Add data file to hold podcast information

---
+++ src/data.ts
@@ -0,0 +1,41 @@
+import { Podcast } from './types';
+import { generateWCMPhotoSize } from './util';
+
+const podcasts: Podcast[] = [
+    {
+        image: 'http://ww4.hdnux.com/photos/70/11/47/14719431/5/rawImage.jpg',
+        title: 'Express Briefing',
+        description: 'The daily briefing from the San Antonio Express-News.',
+        links: [
+            {
+                url: 'https://itunes.apple.com/us/podcast/express-briefing/id1296731191?mt=2',
+                name: 'iTunes'
+            },
+            {
+                url: 'https://www.stitcher.com/podcast/san-antonio-expressnews/express-briefing',
+                name: 'Stitcher'
+            }
+        ]
+    },
+    {
+        image: 'http://ww4.hdnux.com/photos/70/11/47/14719435/4/rawImage.jpg',
+        title: 'EN Depth',
+        description: 'A weekly interview podcast from the San Antonio Express-News.',
+        links: [
+            {
+                url: 'https://itunes.apple.com/us/podcast/en-depth/id1325721282?mt=2',
+                name: 'iTunes'
+            },
+            {
+                url: 'https://www.stitcher.com/s?fid=161142',
+                name: 'Stitcher'
+            }
+        ]
+    }
+];
+
+const podcastImageSet = podcasts.map((podcast) => {
+    return Object.assign({}, podcast, { image: generateWCMPhotoSize(podcast.image) });
+})
+
+export default podcastImageSet;
