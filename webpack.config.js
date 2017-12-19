9899722e266d020e629f5d63ddf60176d4222b16
Kia Farhang <kfarhang0@gmail.com>
2017-12-19 15:00:15 -0600

    Change output JS file name

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './dist'),
         publicPath: '/dist/',
-        filename: 'build.js'
+        filename: 'saen-podcast-page.js'
     },
     module: {
         rules: [
