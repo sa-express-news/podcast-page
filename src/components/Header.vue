d86e1c0e5e3509043c6de93e1db70becf4946fbb
Kia Farhang <kfarhang0@gmail.com>
2017-12-19 14:59:59 -0600

    Add Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="podcast-header">
+      <h1>Express-News Podcasts</h1>
+      <p>We’re transforming written articles into audio read aloud that you can listen to anytime, anywhere. Listen on the go, during your daily commute, or at the gym! Scroll down for links to subscribe using your favorite podcast application.</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
+
+<style>
+.podcast-header h1 {
+  font-size: 30px;
+  line-height: 30px;
+}
+</style>
