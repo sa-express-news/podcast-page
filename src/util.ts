fe14cb96cc48f20c18da6a69d342598cb6a9e9be
Kia Farhang <kfarhang0@gmail.com>
2018-01-26 13:32:29 -0600

    Update WCM photo size converter function to return 800x800 images past 50 500px-wide screens, as podcast images don't need to be any larger than that.

--- src/util.ts
+++ src/util.ts
@@ -9,12 +9,8 @@ export const generateWCMPhotoSize = (imgURL: string): string => {
 
         if (windowWidth <= 500) {
             return replaceRawImageWithSize('500x500');
-        } else if (windowWidth > 500 && windowWidth <= 800) {
-            return replaceRawImageWithSize('800x800');
-        } else if (windowWidth > 800 && windowWidth <= 1200) {
-            return replaceRawImageWithSize('1200x1200');
         } else {
-            return replaceRawImageWithSize('1600x1600');
+            return replaceRawImageWithSize('800x800');
         }
     } else {
         return imgURL;
